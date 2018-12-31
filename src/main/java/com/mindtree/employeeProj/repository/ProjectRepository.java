98230d89c09cc0a16ccdc4e4be1936d05d99c9ea
Anoop Jain <anoop.jain10@gmail.com>
2018-12-31 10:49:50 +0530

    first commit

--- src/main/java/com/mindtree/employeeProj/repository/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mindtree.employeeProj.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.mindtree.employeeProj.entity.Project;
-
-public interface ProjectRepository extends CrudRepository<Project, Integer> {
-
-}
